870e23b57251694eaf9ada4fc814a6f86187bfdf
Emma Popova <emma.p.popova@gmail.com>
2026-08-26 14:58:34 +0300

    test: mock articles API response

--- tests/working-with-apis.spec.ts
+++ tests/working-with-apis.spec.ts
@@ -1,7 +1,6 @@
 import {test, expect} from '@playwright/test';
 import tags from '../test-data/tags.json';
 
-
 test.beforeEach(async({page})=> {
    
       await page.route('*/**/api/tags', async (route)=> {
@@ -11,6 +10,18 @@ test.beforeEach(async({page})=> {
       })
 
     })
+     await page.route('*/**/api/articles*',async route =>{
+        
+        const response = await route.fetch();
+        const responseJSON = await response.json();
+        responseJSON.articles[0].title = 'This is a MOCK title for the article';
+        responseJSON.articles[0].description = 'This is a MOCK description for the article';
+        await route.fulfill({
+            json: responseJSON
+
+        })
+     })
+
     await page.goto('https://conduit.bondaracademy.com/');
 });
 
@@ -18,4 +29,6 @@ test('has title', async({page})=> {
     
     await expect(page.locator('.navbar-brand')).toHaveText(/conduit/);
     await expect(page.locator('.sidebar .tag-pill')).toContainText(['Automation', 'Playwright']);
+    await expect(page.locator('.preview-link h1').first()).toContainText('This is a MOCK title for the article');
+    await expect(page.locator('.preview-link p').first()).toContainText('This is a MOCK description for the article');
 });
