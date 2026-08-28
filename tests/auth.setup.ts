03b1b9280bb65de15d5240100ceeebefb038e1b1
Emma Popova <emma.p.popova@gmail.com>
2026-08-28 13:44:40 +0300

    feat: refresh authentication token in setup

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,9 +1,28 @@
 import {test as setup, expect} from '@playwright/test';
 import path from 'path';
+import fs from 'fs';
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
-setup('authenticate',async({page})=>{
+setup('authenticate',async({page, request})=>{
+    if(fs.existsSync(authFile)){
+        const loginResponse = await request.post('https://conduit-api.bondaracademy.com/api/users/login',{
+       data: {
+         "user": {
+            "email": process.env.TEST_EMAIL,
+            "password": process.env.TEST_PASSWORD
+           }
+         }
+      })
+      expect((loginResponse).status()).toEqual(200);
+      const responseLoginJSON = await loginResponse.json();
+      const token = responseLoginJSON.user.token;
+
+      const storageStateFile = JSON.parse(fs.readFileSync(authFile, 'utf-8'));
+      storageStateFile.origins[0].localStorage[0].value = token;
+      fs.writeFileSync(authFile,JSON.stringify(storageStateFile, null, 2));
+
+    }else {
   
      await page.goto('https://conduit.bondaracademy.com/');
 
@@ -20,7 +39,7 @@ setup('authenticate',async({page})=>{
     await expect(page.getByRole('link', {name: 'New Article'})).toBeVisible();
 
     await page.context().storageState({ path: authFile });
-
+   }
     
 })
 
